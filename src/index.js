e1a97f56fe27a95f3d03b0e936918a8ebdc109fa
Daniel Sidabutar <aguspermadi94@gmail.com>
2018-07-08 00:00:28 +0700

    wrapping main app with Provider to pass the global props

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.css';
+import AppRoute from './containers';
+import store from './store'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={ store }>
+  <AppRoute />
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
+
+// import { render } from 'react-dom';
+// import AppRoute from './components/AppRoute/';
+
+// render(
+// 	<Provider store={ store }>
+// 		<AppRoute />
+// 	</Provider>,
+// 	document.querySelector('#main'));

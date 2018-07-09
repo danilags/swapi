46b21dec0b61dd0b65c161ca9445d4d061729456
Daniel Sidabutar <aguspermadi94@gmail.com>
2018-07-09 20:08:10 +0700

    adding details page

--- src/containers/DetailsPage/index.js
+++ src/containers/DetailsPage/index.js
@@ -1,20 +1,51 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { fetchCharacter } from '../../actions';
+import { GET_DETAILS_CHARACTER } from '../../constants';
+
+import { Wrapper } from '../../components';
 
 class DetailsPage extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      characterId: this.props.match.params.id,
+      character: {}
+    }
+  }
 
+  componentDidMount() {
+    this.props.fetchCharacter({ url: `people/${this.state.characterId}`, type: GET_DETAILS_CHARACTER })
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { characterDetails } = nextProps.dataCharacter
+    if (characterDetails.isFulfilled && this.props.dataCharacter.characterDetails === null) {
+      this.setState({
+        character: characterDetails
+      })
     }
   }
 
   render() {
     return (
-      <div>
-        <h2>Loket Page Details</h2>
-      </div>
+      <Wrapper>
+        <p>{ this.state.character.name }</p>
+        <p>{ this.state.character.birth_year }</p>
+        <p>{ this.state.character.gender }</p>
+        <p>{ this.state.character.eye_color }</p>
+      </Wrapper>
     );
   }
 };
 
-export default DetailsPage;
+const mapStateToProps = state => ({
+  dataCharacter: state.charReducer
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchCharacter: (params) => dispatch(fetchCharacter(params))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(DetailsPage);

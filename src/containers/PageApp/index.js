76fa266ea4f3befd26be60f25a20b89354c78774
Daniel Sidabutar <aguspermadi94@gmail.com>
2018-07-09 20:07:16 +0700

    binding the function onClick event  to Navbar component

--- src/containers/PageApp/index.js
+++ src/containers/PageApp/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Switch } from 'react-router-dom';
 
+import { filterCharacter } from '../../actions';
 import { NavBar } from '../../components';
 import Route from '../Route';
 
 class PageApp extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+    this.toFIlterCharacter = this.toFIlterCharacter.bind(this);
+  }
+
+  toFIlterCharacter(params) {
+    this.props.filterCharacter(params)
+  }
+
   render() {
     return (
       <div>
-        <NavBar />
+        <NavBar 
+          onFilter={this.toFIlterCharacter}
+        />
         <Switch>
           {this.props.routes.map((route, i) => (
               <Route key={i} {...route} />
@@ -22,4 +36,12 @@ class PageApp extends React.Component {
   }
 }
 
-export default PageApp;
+const mapStateToProps = state => ({
+  dataCharacter: state.charReducer
+});
+
+const mapDispatchToProps = dispatch => ({
+  filterCharacter: (params) => dispatch(filterCharacter(params))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(PageApp);
